4127fb6124dce87eedff3e21db4fba974ad99a73
CICERO NEVES <cicero.info@gmail.com>
2017-12-17 19:22:08 -0500

    making run.bat better

--- Factory/run.bat
+++ Factory/run.bat
@@ -1,17 +1,32 @@
 @echo off
+cls
+if not exist classes mkdir classes
+if exist app.jar del app.jar
 
 for %%a in (.) do set currentfolder=%%~na
 echo Project %currentfolder%
+echo Design Pattern - Factory Pattern
+echo In Factory pattern, we create object without exposing the creation logic to the client and refer to newly created object using a common interface.
+echo.
 
-if not exist classes mkdir classes
-
+echo ## Compiling JAVA files ##
 javac -sourcepath src src/*.java -d classes
+echo.
 
+echo ## Running CLASS files ##
 java -cp classes App
+echo.
 
-REM if exist app.jar del app.jar
-REM jar cfve app.jar App ./classes/*.class
-REM java -cp classes -jar app.jar
-REM del app.jar
+
+
+echo ## Creating JAR file ##
+jar cfve app.jar App -C classes .
+echo.
+
+echo ## Running jar file ##
+java -cp classes -jar app.jar
+echo.
+
+del app.jar
 
 rmdir /S /Q classes
